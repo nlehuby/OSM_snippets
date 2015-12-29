2023638e8448bab53b0e15c3926ee8315e1ab3b8
Noémie Lehuby <noemie.lehuby@openmailbox.org>
2015-12-29 18:48:46 +0100

    to_html WIP - simplification
    une seule fonction pour générer tout le nécessaire.
    Il ne reste plus qu'à ajouter une fonction de génération des éventuelles infos manquantes

--- navitia-to-OSM (bus routes)/route_to_html.py
+++ navitia-to-OSM (bus routes)/route_to_html.py
@@ -298,7 +298,105 @@ def render_all():
     #créer l'index
     create_html_index_page()
 
+def to_html():
+    """
+    crée la page d'index listant référençant les pages de chaque parcours OSM
+    """
+
+    #TODO : récréer les fichiers csv à partir des fonctions déjà existantes en complétant les infos manquantes
+
+    osm_csv = csv.reader(open("rendu/sources/osm_parcours.csv", "rb"))
+    navitia_csv = list(csv.reader(open("rendu/sources/navitia_parcours.csv", "rb")))
+
+    autocomplete = {"parcours_osm":[]}
+
+    template_table = ''
+
+    for osm_route in osm_csv:
+        print osm_route[2]
+        #création de l'objet pour l'autocomplétion
+        parcours = {}
+        parcours['value'] = osm_route [0]
+        parcours['label'] = "[{}] {} > {}".format(osm_route[-1], osm_route[1], osm_route[3])
+
+        rapp = [route for route in navitia_csv if route[0] == osm_route[0]] #rapprochement osm navitia
+
+        print (rapp)
+        if rapp != []:
+            print ('ok')
+            parcours['url'] = "bus_route.htm?osm={}&navitia={}".format(osm_route[0], rapp[0][1] )
+
+            #current_osm_route = {'id' : osm_route[0], 'name': osm_route[2], 'ref': osm_route[1], 'nb_stops': osm_route[4]}
+            #current_nav_route = {'id' : rapp[0][1], 'name' : rapp[0][2], 'nb_stops': rapp[0][3]}
+
+            #ajout dans l'index
+            liste_template = """
+                <tr>
+                    <td> %%route_code%%
+                    </td>
+                    <td>
+                    <a href="bus_route.htm?osm=%%relation_id%%&navitia=%%navitia_id%%">%%relation_name%%</a>
+                    </td>
+                    <td>
+                        %%OSM_nb_stops%%/%%navitia_nb_stops%%
+                    </td>
+                    <td>
+
+                        <progress value="%%OSM_nb_stops%%" max="%%navitia_nb_stops%%">état de la carto de la route</progress>
+                    </td>
+
+                <tr>
+                """
+
+            liste_template = liste_template.replace("%%route_code%%", osm_route[1]  )
+            liste_template = liste_template.replace("%%relation_id%%", osm_route[0]  )
+            liste_template = liste_template.replace("%%relation_name%%", osm_route[2]  )
+            liste_template = liste_template.replace("%%OSM_nb_stops%%", osm_route[4]  )
+            liste_template = liste_template.replace("%%navitia_nb_stops%%", rapp[0][3] )
+            liste_template = liste_template.replace("%%navitia_id%%", rapp[0][1] )
+
+
+        else:
+            print ('ko')
+            parcours['url'] = "bus_route.htm?osm={}".format(osm_route[0])
+            liste_template = """
+                <tr>
+                    <td> %%route_code%%
+                    </td>
+                    <td>
+                    <a href="bus_route.htm?osm=%%relation_id%%">%%relation_name%%</a>
+                    </td>
+                    <td colspan=2>
+                        %%OSM_nb_stops%%
+                    </td>
+                <tr>
+                """
+            liste_template = liste_template.replace("%%route_code%%", osm_route[1]  )
+            liste_template = liste_template.replace("%%relation_id%%", osm_route[0]  )
+            liste_template = liste_template.replace("%%relation_name%%", osm_route[2]  )
+            liste_template = liste_template.replace("%%OSM_nb_stops%%", osm_route[4]  )
+
+
+
+        #persistance autocomplétion
+        autocomplete['parcours_osm'].append(parcours)
+        template_table += liste_template
+
+    #persistance de la page d'index
+    now = datetime.datetime.now()
+    mon_fichier = open("rendu/assets/template_liste.html", "r")
+    template = mon_fichier.read()
+    mon_fichier.close()
+    template = template.replace("%%tableau_des_routes%%", template_table  )
+    template = template.replace("%%date_du_jour%%", now.strftime("%d/%m/%Y %H:%M") )
+    mon_fichier = open("rendu/index.html", "wb")
+    mon_fichier.write(template)
+    mon_fichier.close()
+
+    #persistance du fichier d'autocomplétion
+    json.dump(autocomplete, open('rendu/osm_parcours.json', "w"), indent=4)
 
 if __name__ == '__main__':
     #save_geojson_to_file(extract_geojson_from_navitia('route:RTP:1330511_R'))
-    render_all()
+    #render_all()
+    to_html()
